ad37b0702d981c623a6a6e54e70984686a9e116a
Alex <alexgrubor@gmail.com>
2022-11-14 11:50:24 +0100

    simplified pokemon fight  done

---
+++ index.js
@@ -0,0 +1,68 @@
+class Pokemon {
+  constructor(name, health, magic) {
+    this.name = name;
+    this.health = health;
+    this.magic = magic;
+    this.skills = [];
+  }
+  learnAttackSkill = function (attackSkill) {
+    return this.skills.push(attackSkill);
+  };
+  showStatus = function () {
+    if (this.health > 0) {
+      return `${this.name} current health  status:${this.health} current magic status:${this.magic}`;
+    }
+    return `${this.name} is already dead!`;
+  };
+  attack = function (index, pokemon) {
+    if((this.magic >= this.skills[index].magic) && (this.health > 0)) 
+        {
+            this.magic -= this.skills[index].magic;
+            pokemon.health -= this.skills[index].damage;
+
+            return`${this.name} attacked ${pokemon.name} with skill ${this.skills[index].nameOfAttack} successfully!
+            ${pokemon.name} got ${this.skills[index].damage} damage`;
+        }
+        else if(pokemon.health <= 0)
+        {
+           return`${pokemon.name} is killed!`;
+        }
+        else if(this.health <= 0)
+        {
+           return`${this.name} can't attack, he is already dead! 
+           ${pokemon.name} has won this round`;
+        }
+  };
+  getMagic = function () {
+    let randomMagicAddition = Math.floor(Math.random() * 20);
+    this.magic = this.magic + randomMagicAddition;
+    return `${this.name} was add ${randomMagicAddition} amount of magic back`;
+  };
+}
+class AttackSkill {
+  constructor(nameOfAttack, damage, magic) {
+    this.nameOfAttack = nameOfAttack;
+    this.damage = damage;
+    this.magic = magic;
+  }
+}
+// Creating pokemon 
+
+let pikachu = new Pokemon("pikachu", 120, 80);
+let bulbasaur = new Pokemon("bulbasaur", 95, 105);
+// Creating skills
+let lightning = new AttackSkill("lightning", 40, 30);
+let poisonSeed = new AttackSkill("poison seed", 20, 20);
+// Testing methods
+pikachu.learnAttackSkill(lightning);
+bulbasaur.learnAttackSkill(poisonSeed);
+//console.log(pikachu.attack(0, bulbasaur));
+//console.log(bulbasaur.attack(0, pikachu));
+//console.log(pikachu.showStatus());
+//console.log(pikachu.getMagic());
+bulbasaur.attack(0,pikachu)
+console.log(pikachu.showStatus());
+console.log(bulbasaur.showStatus());
+// seeing whole pokemon objects
+//console.log("pikachu :>> ", pikachu);
+//console.log('bulbasaur :>> ', bulbasaur);
---
+++ main.js
@@ -0,0 +1,79 @@
+// starts const
+let attackAmount = Math.floor(Math.random() * 20);
+let magicAmount = Math.floor(Math.random() * 20);
+let recovery= Math.floor(Math.random() * 10);
+let addMagic= Math.floor(Math.random() * 7)
+
+// my buttons
+let attackPlayer1 = document.getElementById('attack1')
+let attackPlayer2 = document.getElementById('attack2')
+let player1HealthRecovery = document.getElementById('health1');
+let player2HealthRecovery = document.getElementById('health2');
+let player1MagicAdd =  document.getElementById('magic1');
+let player2MagicAdd = document.getElementById('magic2');
+
+attackPlayer1.addEventListener('click', () => {
+  if (document.getElementById('player2-magic').value < magicAmount) {
+    alert('You are out of the magic, please refill')
+  }
+  if (document.getElementById("player2-health").value < attackAmount) {
+    alert('Player 2 is dead')
+  }
+  document.getElementById("player2-health").value -= attackAmount
+  document.getElementById('player1-magic').value -= magicAmount
+  let light = document.getElementById('light1')
+  let star = document.getElementById('star1')
+  star.classList.add('active')
+  light.classList.add('active')
+  setTimeout(()=> {
+    light.classList.remove('active')
+    star.classList.remove('active')
+
+  },1000)
+
+})
+attackPlayer2.addEventListener('click', () => {
+  if (document.getElementById("player1-health").value < attackAmount) {
+    alert('Player 1 is dead')
+  }
+  if (document.getElementById('player2-magic').value < magicAmount) {
+    alert('You are out of the magic, please refill')
+  }
+  document.getElementById("player1-health").value -= attackAmount
+  document.getElementById('player2-magic').value -= magicAmount
+  let light = document.getElementById('light2')
+  let star = document.getElementById('star2')
+  star.classList.add('active')
+  light.classList.add('active')
+  setTimeout(()=> {
+    light.classList.remove('active')
+    star.classList.remove('active')
+
+  },1000)
+})
+
+player1HealthRecovery.addEventListener('click', () => {
+  if(document.getElementById("player2-health").value === 100) {
+    alert('Your Health is full, you are ready to fight')
+  }
+  document.getElementById("player1-health").value +=recovery 
+})
+player2HealthRecovery.addEventListener('click', () => {
+  if(document.getElementById("player2-health").value === 100) {
+    alert('Your Health is full, you are ready to fight')
+  }
+  document.getElementById("player2-health").value +=recovery 
+})
+
+player1MagicAdd.addEventListener('click', () => {
+  if (document.getElementById('player1-magic').value === 100) {
+    alert('Your Magic is full,you are ready to fight ')
+  }
+  document.getElementById('player1-magic').value += addMagic
+})
+player2MagicAdd.addEventListener('click', () => {
+  if (document.getElementById('player2-magic').value === 100) {
+    alert('Your Magic is full, you are ready to fight')
+  }
+  document.getElementById('player2-magic').value += addMagic
+})
