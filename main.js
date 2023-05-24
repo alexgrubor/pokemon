c8b3fb4f5813c090b678eca7c226aac354031e85
Alex <alexgrubor@gmail.com>
2023-05-24 19:28:59 +0200

    init

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+let body = document.querySelector('body')
+
 // starts const
-let attackAmount = Math.floor(Math.random() * 20);
-let magicAmount = Math.floor(Math.random() * 20);
-let recovery= Math.floor(Math.random() * 10);
-let addMagic= Math.floor(Math.random() * 7)
+let attackAmount = Math.floor(Math.random() * 20 + 1);
+let magicAmount = Math.floor(Math.random() * 20 +1);
+let recovery= Math.floor(Math.random() * 10 +1);
+let addMagic= Math.floor(Math.random() * 7 + 1)
 
 // my buttons
 let attackPlayer1 = document.getElementById('attack1')
@@ -11,6 +13,8 @@ let player1HealthRecovery = document.getElementById('health1');
 let player2HealthRecovery = document.getElementById('health2');
 let player1MagicAdd =  document.getElementById('magic1');
 let player2MagicAdd = document.getElementById('magic2');
+
+alert('Welcome to the Pokemon lite game')
 
 attackPlayer1.addEventListener('click', () => {
   if (document.getElementById('player2-magic').value < magicAmount) {
@@ -21,6 +25,7 @@ attackPlayer1.addEventListener('click', () => {
   }
   document.getElementById("player2-health").value -= attackAmount
   document.getElementById('player1-magic').value -= magicAmount
+  body.classList.add('attack')
   let light = document.getElementById('light1')
   let star = document.getElementById('star1')
   star.classList.add('active')
@@ -28,7 +33,7 @@ attackPlayer1.addEventListener('click', () => {
   setTimeout(()=> {
     light.classList.remove('active')
     star.classList.remove('active')
-
+   body.classList.remove('attack')
   },1000)
 
 })
@@ -43,12 +48,13 @@ attackPlayer2.addEventListener('click', () => {
   document.getElementById('player2-magic').value -= magicAmount
   let light = document.getElementById('light2')
   let star = document.getElementById('star2')
+  body.classList.add('attack')
   star.classList.add('active')
   light.classList.add('active')
   setTimeout(()=> {
     light.classList.remove('active')
     star.classList.remove('active')
-
+    body.classList.remove('attack')
   },1000)
 })
 
